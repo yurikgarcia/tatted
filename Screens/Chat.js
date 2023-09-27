22119582fbe4c123741948262779f39077290950
yurikgarcia <90262671+yurikgarcia@users.noreply.github.com>
2023-09-27 13:15:34 -0400

    added share features to setting screen

--- Screens/Chat.js
+++ Screens/Chat.js
@@ -1,42 +1,18 @@
-import React, { useState } from 'react';
-import { View, StyleSheet, TouchableOpacity } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
-import { Linking } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Appbar, Avatar, Card } from "react-native-paper";
 
 function Chat() {
-  const [location, setLocation] = useState({
-    latitude: 37.7749, // Replace with the actual latitude
-    longitude: -122.4194, // Replace with the actual longitude
-  });
-
-  const openAppleMaps = () => {
-    const { latitude, longitude } = location;
-    const url = `http://maps.apple.com/?ll=${latitude},${longitude}`;
-    Linking.openURL(url);
-  };
   return (
-    <View style={styles.container}>
-      <TouchableOpacity style={styles.leftHalf} onPress={openAppleMaps}>
-        <MapView
-          style={styles.map}
-          region={{
-            latitude: location.latitude,
-            longitude: location.longitude,
-            latitudeDelta: 0.01,
-            longitudeDelta: 0.01,
-          }}
-        >
-          <Marker
-            coordinate={{
-              latitude: location.latitude,
-              longitude: location.longitude,
-            }}
-            title="Person's Location"
-            description="Click to open in Apple Maps"
-          />
-        </MapView>
-        <View style={styles.gradientOverlay} />
-      </TouchableOpacity>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <View style={styles.circleImage}>
+        <Avatar.Text
+          label="YG"
+          size={50}
+          style={{ backgroundColor: "#0DBB" }}
+          labelStyle={{ fontSize: 24 }}
+        />
+      </View>
     </View>
   );
 }
@@ -44,23 +20,11 @@ function Chat() {
 export default Chat;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row', // Make the parent container a row to split the screen in half
-  },
-  leftHalf: {
-    flex: 1, // Take up half of the available width
-    backgroundColor: 'transparent',
-  },
-  map: {
-    flex: 1,
-  },
-  gradientOverlay: {
-    position: 'absolute',
-    left: '35%', // Cover 25% of the map's width
-    top: 0,
-    bottom: 0,
-    right: 0,
-    backgroundColor: 'linear-gradient(90deg, rgba(0, 0, 0, 0.7) 35%, rgba(0, 0, 0, 0) 35%)',
+  circleImage: {
+    borderRadius: 50,
+    height: 50,
+    marginLeft: 10,
+    overflow: "hidden",
+    width: 50,
   },
 });
--- Screens/Settings.js
+++ Screens/Settings.js
@@ -1,12 +1,144 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { View, Text, Share, StyleSheet, TouchableOpacity } from "react-native";
+import { Avatar } from "react-native-paper";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 function Settings() {
+  const shareWithFriends = async () => {
+    try {
+      const result = await Share.share({
+        message: "Check out this cool app I use to find new tattoo artists!", // Your sharing message
+        url: "https://tatted.io", // URL to your app or any specific content
+      });
+
+      if (result.action === Share.sharedAction) {
+        // Sharing was successful
+        console.log("Shared successfully");
+      } else if (result.action === Share.dismissedAction) {
+        // Sharing was dismissed
+        console.log("Sharing dismissed");
+      }
+    } catch (error) {
+      console.error("Error sharing:", error);
+    }
+  };
+
+  const shareWithArtists = async () => {
+    try {
+      const result = await Share.share({
+        message: "Check out this cool app for connecting artists with clients!", // Your sharing message
+        url: "https://tatted.io", // URL to your app or any specific content
+      });
+
+      if (result.action === Share.sharedAction) {
+        // Sharing was successful
+        console.log("Shared successfully");
+      } else if (result.action === Share.dismissedAction) {
+        // Sharing was dismissed
+        console.log("Sharing dismissed");
+      }
+    } catch (error) {
+      console.error("Error sharing:", error);
+    }
+  };
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Look at ur dumb setting!</Text>
+    <View style={styles.container}>
+      <View style={styles.userInfo}>
+        <View style={styles.circleImage}>
+          <Avatar.Text
+            label="YG"
+            size={50}
+            style={{ backgroundColor: "#0DBB" }}
+            labelStyle={{ fontSize: 24 }}
+          />
+        </View>
+        <View style={styles.userInfoText}>
+          <Text style={styles.heading}>Yurik Garcia</Text>
+          <Text>EDIT ACCOUNT</Text>
+        </View>
+      </View>
+      <View style={styles.settingsText}>
+        <TouchableOpacity
+          style={styles.settingsText}
+          onPress={shareWithFriends} // Call the sharing function when pressed
+        >
+          <MaterialCommunityIcons name="share-all" size={24} color="black" />
+          <Text style={styles.textStyle}>Share With Friends</Text>
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity
+        onPress={shareWithArtists} // Call the sharing function when pressed
+      >
+        <View style={styles.settingsText}>
+          <MaterialCommunityIcons
+            name="account-arrow-right"
+            size={24}
+            color="black"
+          />
+          <Text style={styles.textStyle}>Invite An Artist</Text>
+        </View>
+      </TouchableOpacity>
+      <View style={styles.settingsText}>
+        <MaterialCommunityIcons name="ticket" size={24} color="black" />
+        <Text style={styles.textStyle}>Vouchers</Text>
+      </View>
+      <View style={styles.settingsText}>
+        <MaterialCommunityIcons
+          name="email-newsletter"
+          size={24}
+          color="black"
+        />
+        <Text style={styles.textStyle}>Feedback</Text>
+      </View>
+      <View style={styles.settingsText}>
+        <MaterialCommunityIcons name="logout" size={28} color="red" />
+        <Text style={styles.logoutStyle}>Log Out</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    marginLeft: 10,
+    marginTop: 10,
+  },
+  userInfo: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  circleImage: {
+    borderRadius: 50,
+    height: 50,
+    width: 50,
+    backgroundColor: "#0DBB",
+    marginRight: 10,
+  },
+  userInfoText: {
+    flexDirection: "column",
+  },
+  heading: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  settingsText: {
+    marginTop: 20, // Adjust the spacing as needed
+    flexDirection: "row",
+  },
+  textStyle: {
+    marginLeft: 5,
+    marginTop: 2,
+    fontSize: 16,
+  },
+  logoutStyle: {
+    marginLeft: 5,
+    marginTop: 3,
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
 
 export default Settings;
